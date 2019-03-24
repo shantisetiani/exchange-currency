228b20cfad112a3183a4d4699252d65eeb617cb6
Shanti Setiani <shanti.setiani@ralali.com>
2019-03-24 17:00:36 +0700

    add some missing file

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -2,6 +2,11 @@ export class Currency {
     constructor(
         public base: string,
         public date: Date,
-        public rates: string,
+        public rates: Rates,
+    ) { }
+}
+export class Rates {
+    constructor(
+        public rates: Float32Array
     ) { }
 }
